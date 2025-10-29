253bbe13c016a28d29bafeef4e42fef455803fb9
Christian <christianfryksten1@gmail.com>
2025-10-29 18:28:25 +0100

    Starter files

---
+++ src/test/java/com/spring_boot_course/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_course.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
