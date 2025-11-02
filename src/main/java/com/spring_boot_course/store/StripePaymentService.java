25ace5fb709cbd00ea4060567aaa8c905a95bd03
Christian <christianfryksten1@gmail.com>
2025-11-02 13:58:03 +0100

    change config to yaml

--- src/main/java/com/spring_boot_course/store/StripePaymentService.java
+++ src/main/java/com/spring_boot_course/store/StripePaymentService.java
@@ -1,15 +1,31 @@
 package com.spring_boot_course.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("stripe")
 @Primary
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}") // Injects value from application.properties into field variable
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.timeout:3000}") // Sets timeout to 3000 if its value is undefined
+    private int timeout;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> supportedCurrencies;
 
     @Override
     public void processPayment(double amount) {
         System.out.println("STRIPE");
+        System.out.println("API URL " + apiUrl);
+        System.out.println("Currencies: " + supportedCurrencies);
         System.out.println("Amount: " + amount);
     }
 }
