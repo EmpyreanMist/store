088fbf9164f147b46812a2af65bd54b35f4cf135
Christian <christianfryksten1@gmail.com>
2025-11-05 20:34:15 +0100

    Code cleanup

--- src/main/java/com/spring_boot_course/store/StoreApplication.java
+++ src/main/java/com/spring_boot_course/store/StoreApplication.java
@@ -1,7 +1,5 @@
 package com.spring_boot_course.store;
 
-import com.spring_boot_course.store.entities.Profile;
-import com.spring_boot_course.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,22 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StoreApplication {
 
     public static void main(String[] args) {
-
         ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-        User user = new User(
-                "Christian",
-                "test@test.com",
-                "123"
-        );
-
-        Profile profile = new Profile();
-        profile.setBio("bio");
-
-        // connect both sides
-        user.setProfile(profile);
-        profile.setUser(user);
-
-        System.out.println(user);
     }
 }
